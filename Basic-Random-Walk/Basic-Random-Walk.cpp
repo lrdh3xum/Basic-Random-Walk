5a18965dc3d37a0dd67f5e21b8a2209aeccc2c8a
Brandon <fsc7p2ioxntf@protonmail.com>
2020-11-13 12:29:11 -0500

    Final commit
    Finalized a basic Monte Carlo process.

--- Basic-Random-Walk/Basic-Random-Walk.cpp
+++ Basic-Random-Walk/Basic-Random-Walk.cpp
@@ -1,7 +1,82 @@
-
 #include <iostream>
+#include "RandomWalkGenerator.h"
+#include <cstdlib>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+RandomWalkGenerator::RandomWalkGenerator(int size, double start, double step)
+    : m_numSteps(size),
+    m_stepSize(step),
+    m_initialPrice(start)
+{}
+
+RandomWalkGenerator::RandomWalkGenerator(const RandomWalkGenerator& p)
+    : m_numSteps(p.m_numSteps),
+    m_stepSize(p.m_numSteps),
+    m_initialPrice(p.m_initialPrice)
+{}
+
+RandomWalkGenerator::~RandomWalkGenerator()
+{}
+
+RandomWalkGenerator& RandomWalkGenerator::operator=(const RandomWalkGenerator& p)
+{
+    if (this != &p)
+    {
+        m_numSteps = p.m_numSteps;
+        m_initialPrice = p.m_initialPrice;
+        m_stepSize = p.m_stepSize;
+    }
+    return *this;
+}
+
+// Get a step value.
+double RandomWalkGenerator::computeRandomStep(double currentPrice)
+{
+    const int num_directions = 3;
+
+    // There are better ways to do this than with rand().
+    int r = rand() % num_directions;
+    double val = currentPrice;
+
+    if (r == 0)
+    {
+        val += (m_stepSize * val);
+    }
+    else if (r == 1)
+    {
+        val -= (m_stepSize * val);
+    }
+    return val;
+}
+
+// Generate random values within the constraints of the constructor.
+std::vector<double> RandomWalkGenerator::generateWalk()
+{
+    vector<double> walk;
+    double prev = m_initialPrice;
+
+    for (int i = 0; i < m_numSteps; i++)
+    {
+        double val = computeRandomStep(prev);
+        walk.push_back(val);
+        prev = val;
+    }
+    return walk;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Testing 100 steps starting at $30.00
+    RandomWalkGenerator rw(100, 30, 0.01);
+    vector<double> walk = rw.generateWalk();
+
+    for (int i = 0; i < walk.size(); i++)
+    {
+        cout << ", " << i << ", " << walk[i] << endl;
+    }
+    cout << endl;
+    return 0;
 }
--- Basic-Random-Walk/RandomWalkGenerator.h
+++ Basic-Random-Walk/RandomWalkGenerator.h
@@ -1,6 +1,36 @@
 #ifndef __RandomWalkGenerator__
 #define __RandomWalkGenerator__
 
+#include <vector>
+
+//
+// Random walk class for price simulation.
+//
+class RandomWalkGenerator
+{
+public:
+	// Constructors.
+	RandomWalkGenerator(int size, double start, double step);
+	RandomWalkGenerator(const RandomWalkGenerator& p);
+
+	// Destructor.
+	~RandomWalkGenerator();
+
+	// Assignment.
+	RandomWalkGenerator& operator=(const RandomWalkGenerator& p);
+
+	// Return vector with the values of the walk.
+	std::vector<double> generateWalk();
+
+	// Get single step.
+	double computeRandomStep(double currentPrice);
+
+private:
+	// number of steps, size of each step (%), starting priace.
+	int m_numSteps;
+	double m_stepSize;
+	double m_initialPrice;
+};
 
 
 #endif
